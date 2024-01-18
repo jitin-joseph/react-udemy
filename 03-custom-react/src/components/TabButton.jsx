5618d9189e3d6b612ac430bb18365ccd0f9919f8
Jitin Joseph <jitin.joseph@toptechinfo.com>
2024-01-18 16:41:54 +0530

    dynamic selection elemnts

--- 03-custom-react/src/components/TabButton.jsx
+++ 03-custom-react/src/components/TabButton.jsx
@@ -1,10 +1,10 @@
-export default function TabButton({children,onSelect}){
+export default function TabButton({children,onSelect,isSelected}){
     console.log("TabButton component executing");
     function handleClick(){
         console.log("inside click of tab button event");
     }
 
     return (
-        <li><button onClick={onSelect}>{children}</button></li>
+        <li><button className={isSelected ? 'active':''} onClick={onSelect}>{children}</button></li>
     )
 }
