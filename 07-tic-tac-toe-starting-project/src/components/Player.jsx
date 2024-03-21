251a0976052c31b2721f2407cdf086f4f9b3aedf
Jitin Joseph <jitin.joseph@toptechinfo.com>
2024-03-21 18:46:52 +0530

    component condition based chnages

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export default function Player({name, symbol}) {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing(true);
+    }
+
+    let playername = <span className="player-name" >{name}</span>;
+
+    if (isEditing) {
+        playername = <input className="player-name" type="text"  />;
+    }
+
     return (
         <li>
           <span className="player">
-          <span className="player-name">{name}</span>
+          {playername}
           <span className="player-symbol">{symbol}</span>
           </span>
-          <button>Edit</button>
+          <button onClick={handleEditClick}>Edit</button>
         </li>
     );
 }
