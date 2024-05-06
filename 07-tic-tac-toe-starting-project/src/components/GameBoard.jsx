4ed220158bc5a056ba2353ee8567de6936d49340
Jitin Joseph <jitin.joseph@toptechinfo.com>
2024-05-06 17:32:06 +0530

    Lifting State Up

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -6,15 +6,19 @@ const initalGameBoard = [
     [null, null,null]
 ];
 
-export default function GameBoard(){
+export default function GameBoard({onSelectSquare,activePlayerSymbol}){
     const [gameBoard, setGameBoard] = useState(initalGameBoard);
+
     function handleSelectSquare(rowIndex, colIndex){
         setGameBoard((prevGameBoard) => {
             const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = 'X';    
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;    
             return updatedBoard;
         });
+
+        onSelectSquare();
     }
+
     return (<ol id="game-board">
         {gameBoard.map((row, rowIndex) => {
             return <li key={rowIndex}>
